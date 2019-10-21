95246265e2074f1fb6a849c72883f76f9ced9081
PedroRamosRguez <p.ramos1990@gmail.com>
2019-10-21 21:39:14 +0100

    chatbot box almost ready

--- components/chatBot.vue
+++ components/chatBot.vue
@@ -14,8 +14,46 @@
       <div class="footer text-center">
         <span class="foo-txt">
           <br>
-          Done <i class="fas fa-heart"></i>for JSDayCan2019
+          Done <i class="fas fa-heart" />for JSDayCan2019
         </span>
+      </div>
+    </div>
+    <div id="chatCont" class="chatCont" :style='{"display":(chatContDivIsActive ? "None":"block")}'>
+      <div class="bot_profile" :style='{"display":(chatContDivIsActive ? "None":"block")}'>
+        <img src="https://cdn1.iconfinder.com/data/icons/game-design-butterscotch-vol-2/256/Sprite-512.png" class="bot_p_img">
+        <div class="close" @click="toggleClass()">
+          cerrar
+          <i class="fa fa-times" aria-hidden="true" />
+        </div>
+      </div><!--bot_profile end-->
+      <div id="result_div" class="resultDiv">
+        <!-- <p :class="text.class" v-for="text in chatBox"> {{ text.value }}</p><div class="clearfix"></div>  -->
+      </div>
+      <div id="chat-div" class="chatForm">
+        <div class="spinner">
+          <div class="bounce1" />
+          <div class="bounce2" />
+          <div class="bounce3" />
+        </div>
+      </div>
+      <!-- Añadir la funcion para enviar al servidor... -->
+      <input
+        id="chat-input"
+        v-model="text"
+        type="text"
+        class="form-control bot-txt"
+        autocomplete="off"
+        placeholder="Try typing here"
+        @keyup.enter="sendText(text)"
+      />
+    </div>
+    <div class="profile_div" :style='{"display":(!profileDivIsActive?"block":"None")}' @click="toggleClass()">
+      <div class="row">
+        <div class="col-hgt">
+          <div class="chat-txt">
+            <font-awesome-icon icon="comments" style="font-size: 24px;"></font-awesome-icon>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -84,4 +122,280 @@ a:focus {
 .fa-heart {
   color: #fb2323;
 }
+/* MyBot */
+.center .chatCont,
+.center .profile_div {
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+}
+.left .chatCont,
+.left .profile_div {
+  left: 0;
+}
+.chatCont {
+  position: fixed;
+  width: 400px;
+  height: 400px !important;
+  border: 1px solid #dcdcdc;
+  bottom: 65px;
+  right: 0;
+  background: #efe7dd;
+  border-top-left-radius: 3px;
+  z-index: 10000;
+  display: none;
+  /* opacity: 0.4; */
+}
+.close {
+  position: absolute;
+  right: 5px;
+  margin-top: -60px;
+  color: #868686;
+  font-size: 18px;
+  cursor: pointer;
+}
+.bot_profile {
+  position: absolute;
+  right: 0;
+  margin-top: -45px;
+  text-align: center;
+  width: 400px;
+  display: none;
+  z-index: 1000;
+}
+.bot_p_img {
+  width: 80px;
+  border-radius: 50%;
+  width: 80px;
+  border-radius: 50%;
+  background: #fff;
+  box-shadow: 0 0px 1px 0 rgba(0,0,0,0.16), 0 0px 10px 0 rgba(0,0,0,0.12);
+}
+.chatForm {
+  position: absolute;
+  bottom: -65px;
+  right: 0;
+  margin-bottom: 0;
+  width: 400px;
+  display: none;
+}
+.bot-txt,
+.bot-txt:focus {
+  box-shadow: none;
+  border-radius: 0;
+  height: 66px;
+  border: 0;
+  border-top: 1px solid #dcdcdc;
+  border-left: 1px solid #dcdcdc;
+}
+.resultDiv .botResult {
+  background: #fff;
+  color: #000;
+  border-radius: 3px;
+  padding: 12px;
+  font-size: 14px;
+  font-weight: 400;
+  width: 400px;
+  float: left;
+  box-shadow: 0 0px 1px 0 rgba(0,0,0,0.16),0 0px 10px 0 rgba(0,0,0,0.12);
+  border-top-left-radius: 0;
+  margin-bottom: 10px;
+  margin-top: 5px;
+  width: 100%;
+  position: relative;
+}
+.resultDiv .botResult:before {
+  content: "";
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-top: 0px solid rgba(0, 0, 0, 0);
+  border-bottom: 15px solid rgba(0, 0, 0, 0);
+  border-right: 15px solid #fff;
+  left: -15px;
+  margin-top: -12px;
+}
+.resultDiv .userEnteredText {
+  background: #9C27B0;
+  float: right;
+  color: #ffffff;
+  border-radius: 3px;
+  padding: 12px;
+  font-size: 15px;
+  font-weight: 400;
+  width: 400px;
+  box-shadow: 0 0px 1px 0 rgba(0,0,0,0.16),0 0px 10px 0 rgba(0,0,0,0.12);
+  border-top-right-radius: 0;
+  position: relative;
+  left: -6px;
+  margin-bottom: 10px;
+  margin-top: 5px;
+  width: 100%;
+  word-wrap: break-word;
+}
+.resultDiv .userEnteredText:before {
+  content: "";
+  width: 0;
+  height: 0;
+  border-top: 0px solid rgba(0, 0, 0, 0);
+  border-bottom: 15px solid rgba(0, 0, 0, 0);
+  border-left: 15px solid #9C27B0;
+  position: absolute;
+  right: -15px;
+  margin-top: -12px;
+}
+.profile_div {
+  position: fixed;
+  bottom: 0;
+  right: -175px;
+  width: 240px;
+  cursor: pointer;
+}
+.img-profile {
+  width: 65px;
+  box-shadow: 0 0px 1px 0 rgba(0,0,0,0.16), 0 0px 10px 0 rgba(0,0,0,0.12);
+  background: white;
+  border-radius: 50px;
+}
+.col-hgt {
+  height: 100px;
+  display: flex;
+  align-items: center;
+  float: left;
+  margin-right: 10px;
+}
+.chat-txt {
+  box-shadow: 0 0px 1px 0 rgba(0,0,0,0.16),0 0px 10px 0 rgba(0,0,0,0.12);
+  padding: 15px 15px;
+  border-radius: 50px;
+  background: #FF9800;
+  float:right;
+  /* width:100px; */
+}
+/* Spinner start */
+.spinner {
+  background: #9C27B0;
+  width: 70px;
+  text-align: center;
+  display: none;
+  border-radius: 4px;
+  position: relative;
+  top: -7px;
+  left: 19px;
+  padding: 7px 10px;
+  border-top-left-radius: 0;
+}
+.spinner:before {
+  content: "";
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-top: 0px solid rgba(0, 0, 0, 0);
+  border-bottom: 15px solid rgba(0, 0, 0, 0);
+  border-right: 15px solid #9C27B0;
+  left: -15px;
+  margin-top: -7px;
+}
+.spinner > div {
+  width: 10px;
+  height: 10px;
+  background-color: #fff;
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  margin-right: 5px;
+}
+.spinner .bounce1 {
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.spinner .bounce2 {
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+@-webkit-keyframes sk-bouncedelay {
+  0%, 80%, 100% { -webkit-transform: scale(0) }
+  40% { -webkit-transform: scale(1.0) }
+}
+@keyframes sk-bouncedelay {
+  0%, 80%, 100% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  } 40% {
+    -webkit-transform: scale(1.0);
+    transform: scale(1.0);
+  }
+}
+/* Spinner End */
+/* Suggestions */
+.suggestion {
+  background: #fff;
+  color: #000;
+  border-radius: 3px;
+  padding: 12px;
+  font-size: 14px;
+  font-weight: 400;
+  width: 400px;
+  float: left;
+  box-shadow: 0 0px 1px 0 rgba(0,0,0,0.16), 0 0px 10px 0 rgba(0,0,0,0.12);
+  margin-left: 5px;
+  border-top-left-radius: 0;
+  margin-bottom: 20px;
+  width: 100%;
+  position: relative;
+}
+.suggestion:before {
+  content: "";
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-top: 0px solid rgba(0, 0, 0, 0);
+  border-bottom: 15px solid rgba(0, 0, 0, 0);
+  border-right: 15px solid #fff;
+  left: -15px;
+  margin-top: -12px;
+}
+.suggestion .sugg-options {
+  display: block;
+  background: #9c27b0;
+  color: #fff;
+  text-align: center;
+  padding: 5px;
+  margin-bottom: 5px;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: 300;
+}
+.suggestion .sugg-title {
+  font-size: 15px;
+  font-weight: 300;
+  margin-bottom: 10px;
+}
+.suggestion span:last-child {
+  margin-bottom: 0;
+}
+/* Suggestions end */
+.resultDiv {
+  padding: 35px 20px 30px 20px;
+  overflow-y: auto;
+  height: 100%;
+}
+@media screen and (max-width: 500px) {
+  .resultDiv .userEnteredText,
+  .resultDiv .botResult,
+  .suggestion {
+    width: 90%;
+    margin-bottom: 20px;
+  }
+  .chatCont,
+  .chatForm {
+    width: 100%;
+  }
+  .chatCont {
+    height: 70% !important;
+  }
+}
+/* MyBot end */
 </style>
